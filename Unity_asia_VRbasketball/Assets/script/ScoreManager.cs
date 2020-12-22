82a1ded41151c779f8738b8f888d6240f84b31a2
moomu-owo <71679979+moomu-owo@users.noreply.github.com>
2020-12-22 12:00:54 +0800

    喔!分數管理氣氣氣氣氣

---
+++ Unity_asia_VRbasketball/Assets/script/ScoreManager.cs
@@ -0,0 +1,18 @@
+﻿
+using UnityEngine;
+using UnityEngine.UI;
+
+
+public class ScoreManager : MonoBehaviour
+{
+    [Header("分數介面")]
+    public Text textScore;
+    [Header("分數")]
+    public int score;
+
+    private void OnTriggerExit(Collider other)
+    {
+        print(other.name);
+    }
+}
+
