ce0cfb431d11b691428fcfed8faa31c771ff7709
moomu-owo <71679979+moomu-owo@users.noreply.github.com>
2020-12-29 12:00:51 +0800

    1229三分球，加入音效

--- Unity_asia_VRbasketball/Assets/script/ScoreManager.cs
+++ Unity_asia_VRbasketball/Assets/script/ScoreManager.cs
@@ -9,13 +9,51 @@ public class ScoreManager : MonoBehaviour
     public Text textScore;
     [Header("分數")]
     public int score;
+    [Header("投進的分數")]
+    public int scoreIn = 2;
+    [Header("進球音效")]
+    public AudioClip soundIn;
 
+    private AudioSource aud;
+
+    private void Awake()
+    {
+        //音效來源 = 取得元件<音效來源>()
+        aud = GetComponent<AudioSource>();
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+         //如果 碰撞物件的標籤 為 籃球 就加分
+        if (other.tag == "basketball")
+        {
+            AddScore();
+        }
+        // 如果 碰撞的跟物件名稱為  Player
+
+
+        if (other.transform.root.name == "Player")
+        {
+            //玩家進入三分區域，將投進分數改為三分
+            scoreIn = 3;
+        }
+    }
+    // 當碰撞物件離開碰撞範圍時執行一次
     private void OnTriggerExit(Collider other)
     {
-        if (other.tag == "籃球")
+        if (other.transform.root.name =="Player")
         {
-            print("加分");
+            //將投進的分數改為兩分
+            scoreIn = 2;
         }
+    }
+
+    ///加分數
+    private void AddScore()
+    {
+        score += scoreIn;                                                                             //分數遞增
+        textScore.text = "Score : 0" + score;                                           //更新介面
+        aud.PlayOneShot(soundIn, Random.Range(1f, 5f));               //音效來源.撥放一次(音效片段，音量)
     }
 }
 
